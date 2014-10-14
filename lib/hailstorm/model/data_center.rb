3175b4c036bc942d312fc8232d86d7c4ca414299
Ravish Kumar Tiwari <ravishktiwari@hotmail.com>
2014-10-14 19:26:59 +0530

    changes in data_center modal

--- lib/hailstorm/model/data_center.rb
+++ lib/hailstorm/model/data_center.rb
@@ -31,7 +31,6 @@ class Hailstorm::Model::DataCenter < ActiveRecord::Base
     # create load agents
 
     #TODO check master slave
-    self.provision_agents()
     status = true
     self.machines.each do |machine|
       #check machine status
@@ -44,6 +43,7 @@ class Hailstorm::Model::DataCenter < ActiveRecord::Base
     if status
       if self.active?
         self.save!()
+        provision_agents()
       else
         self.update_column(:active, false) if self.persisted?
       end
@@ -55,10 +55,23 @@ class Hailstorm::Model::DataCenter < ActiveRecord::Base
   # start the agent and update agent ip_address and identifier
   def start_agent(load_agent)
     logger.debug { "#{self.class}##{__method__}" }
+    # SSH is available a while later even though status may be running
+    logger.debug { load_agent.attributes.inspect }
+    if load_agent.private_ip_address.nil?
+      ip = self.machines.shift()
+      # update attributes
+      load_agent.identifier = ip
+      load_agent.public_ip_address = ip
+      load_agent.private_ip_address = ip
+
       # SSH is available a while later even though status may be running
-      logger.info { "agent##{load_agent.public_ip_address} is running, ensuring SSH access..." }
-      Hailstorm::Support::SSH.ensure_connection(load_agent.public_ip_address,
+      puts "agent##{load_agent.private_ip_address} is running, ensuring SSH access with #{self.user_name}..."
+      sleep(30)
+      logger.debug { "sleep over..."}
+      Hailstorm::Support::SSH.ensure_connection(load_agent.private_ip_address,
                                                 self.user_name, ssh_options)
+
+    end
   end
 
   # stop the load agent
@@ -78,8 +91,8 @@ class Hailstorm::Model::DataCenter < ActiveRecord::Base
   def before_generate_load()
     logger.debug { "#{self.class}##{__method__}" }
     self.load_agents.where(:active => true).each do |agent|
-      unless agent.running?
-        #start_agent(agent)
+      unless agent.private_ip_address.nil?
+        start_agent(agent)
         agent.save!
       end
     end
@@ -94,8 +107,6 @@ class Hailstorm::Model::DataCenter < ActiveRecord::Base
   def before_destroy_load_agent(load_agent)
     logger.debug { "#{self.class}##{__method__}" }
   end
-
-
 
   # (see Hailstorm::Behavior::Clusterable#slug)
   def slug()
@@ -113,97 +124,9 @@ class Hailstorm::Model::DataCenter < ActiveRecord::Base
     end
   end
 
-  # Launch new instances or bring down instances based on number of instances needed
-  def provision_agents()
-    puts "Inside #{self.class}##{__method__}"
-    logger.debug { "#{self.class}##{__method__}" }
-    self.project.jmeter_plans.where(:active => true).each do |jmeter_plan|
-      common_attributes = {
-          :jmeter_plan_id => jmeter_plan.id,
-          :active => true
-      }
-      required_count = required_load_agent_count()
-
-      if self.project.master_slave_mode?
-        query = self.master_agents
-        .where(:jmeter_plan_id => jmeter_plan.id)
-
-        # abort if more than 1 master agent is present
-        if query.all.count > 1
-          raise(Hailstorm::MasterSlaveSwitchOnConflict)
-        end
-
-        # one master is necessary
-        query.first_or_create!()
-        .tap {|agent| agent.update_column(:active, true)}
-
-        if required_count > 1
-          create_or_enable(common_attributes, required_count, :slave_agents)
-        end
-
-      else # not operating in master-slave mode
-        # abort if slave agents are present
-        slave_agents_count = self.slave_agents
-        .where(:jmeter_plan_id => jmeter_plan.id)
-        .all.count()
-        if slave_agents_count > 0
-          raise(Hailstorm::MasterSlaveSwitchOffConflict)
-        end
-
-        begin
-          create_or_enable(common_attributes, required_count, :master_agents)
-        rescue Exception => e
-          logger.error(e.message)
-          logger.debug { "\n".concat(e.backtrace().join("\n")) }
-          raise(Hailstorm::AgentCreationFailure)
-        end
-      end
-    end
-  end
-
   ######################### PRIVATE METHODS ####################################
   private
   #################### WE ARE NOY USING KEY FOR NOW SO WE DON'T NEED THESE FUNCTIONS
-
-  # Creates master/slave agents based on the relation
-  def create_or_enable(attributes, required_count, relation)
-
-    # count of active agents
-    active_count = self.send(relation).where(attributes).all().count()
-
-    activate_count = required_count - active_count
-
-    activate_count.times do # block wont execute if activate_count <= 0
-
-      # is there an inactive agent? if yes, activate it, or, create new
-      agent = self.send(relation)
-      .where(attributes.merge(:active => false))
-      .first_or_initialize(:active => true)
-      if agent.new_record?
-        if activate_count > 1
-          Hailstorm::Support::Thread.start(agent) do |agent_instance|
-            #start_agent(agent_instance)
-            agent_instance.save!
-          end
-        else
-          #start_agent(agent)
-          agent.save!
-        end
-      else
-        agent.update_column(:active, true)
-      end
-    end
-    Hailstorm::Support::Thread.join() if activate_count > 1# wait for agents to be created
-
-    if activate_count < 0
-      self.send(relation)
-      .where(attributes)
-      .limit(activate_count.abs).each do |agent|
-
-        agent.update_column(:active, false)
-      end
-    end
-  end
 
   def identity_file_exists?
     return File.exists?(identity_file_path)
@@ -223,11 +146,9 @@ class Hailstorm::Model::DataCenter < ActiveRecord::Base
   end
 
   def machine_status?(ip_address)
-    #check if Hailstorm can SSH to specified machine
-    #logger.info("Checking SSH connectivity status #{ip_address}")
+    #TODO check if Hailstorm can SSH to specified machine, check Java and jMeter
 
-    status = Hailstorm::Support::SSH.ensure_connection(ip_address,
-                                                               self.user_name, ssh_options)
+    status = true
     return status
   end
 
@@ -266,11 +187,11 @@ class Hailstorm::Model::DataCenter < ActiveRecord::Base
     return jmeter_available
   end
 
-  def java_version()
+  def java_version_ok?
 
   end
 
-  def jmeter_version()
+  def jmeter_version_ok?
 
   end
 
