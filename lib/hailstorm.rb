cc9faf219b9797d26ffef608ac508e2bd5aecf06
Ravish Kumar Tiwari <ravishktiwari@hotmail.com>
2014-10-29 15:54:53 +0530

    Fixed issue with ActionView.Render while create new hailstorm project

--- lib/hailstorm.rb
+++ lib/hailstorm.rb
@@ -3,6 +3,7 @@ require 'java'
 require 'ostruct'
 require 'active_support/all'
 require 'active_record'
+require 'action_dispatch/http/mime_type'
 require 'action_view'
 
 require "hailstorm/version"
