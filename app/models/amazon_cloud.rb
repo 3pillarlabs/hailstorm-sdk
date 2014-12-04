09948141bf2a67201e17833000d6e8fcd1d56cbc
eash khatri <eash.khatri@3pillarglobal.com>
2014-12-04 13:36:13 +0530

    changes to fix validation errors

--- app/models/amazon_cloud.rb
+++ app/models/amazon_cloud.rb
@@ -1,3 +1,3 @@
 class AmazonCloud < Cluster
-
+  validates :access_key, :secret_key, :region, :instance_type, presence: true
 end
--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -19,53 +19,53 @@ class Cluster < ActiveRecord::Base
   AMAZON_CLUSTER_REGIONS = {"us-east-1"=>"us-east-1","us-west-1"=>"us-west-1","us-west-2"=>"us-west-2","eu-west-1"=>"eu-west-1","ap-northeast-1"=>"ap-northeast-1","ap-southeast-1"=>"ap-southeast-1","sa-east-1"=>"sa-east-1"}
   AMAZON_INSTANCE_TYPES = {"m1.small"=>"m1.small","m1.large"=>"m1.large","m1.xlarge"=>"m1.xlarge","c1.xlarge"=>"c1.xlarge"}
 
-  def check_form_for_spoofed_and_mandatory_data
-    if title.present? and !(title == "AmazonCloud" or name == "DataCenter")
-      errors.add(:name, "can't change cluster name")
-    elsif name == "AmazonCloud"
-
-      if !access_key.present?
-        errors.add(:access_key, " can't be blank")
-      end
-
-      if !secret_key.present?
-        errors.add(:secret_key, " can't be blank")
-      end
-
-      if !region.present?
-        errors.add(:region, " can't be blank")
-      end
-
-      if !instance_type.present?
-        errors.add(:instance_type, " can't be blank")
-      end
-
-      if !AMAZON_CLUSTER_REGIONS.has_value?(region)
-        errors.add(:region, "must be from the list")
-      end
-
-      if !AMAZON_INSTANCE_TYPES.has_value?(instance_type)
-        errors.add(:instance_type, "must be from the list")
-      end
-    elsif name == "DataCenter"
-      if !user_name.present?
-        errors.add(:user_name, " can't be blank")
-      end
-
-      if !machines.present? or machines == "[]"
-        errors.add(:machines, " can't be blank")
-      end
-    end
-
-  end
+  # def check_form_for_spoofed_and_mandatory_data
+  #   if title.present? and !(title == "AmazonCloud" or name == "DataCenter")
+  #     errors.add(:name, "can't change cluster name")
+  #   elsif name == "AmazonCloud"
+  #
+  #     if !access_key.present?
+  #       errors.add(:access_key, " can't be blank")
+  #     end
+  #
+  #     if !secret_key.present?
+  #       errors.add(:secret_key, " can't be blank")
+  #     end
+  #
+  #     if !region.present?
+  #       errors.add(:region, " can't be blank")
+  #     end
+  #
+  #     if !instance_type.present?
+  #       errors.add(:instance_type, " can't be blank")
+  #     end
+  #
+  #     if !AMAZON_CLUSTER_REGIONS.has_value?(region)
+  #       errors.add(:region, "must be from the list")
+  #     end
+  #
+  #     if !AMAZON_INSTANCE_TYPES.has_value?(instance_type)
+  #       errors.add(:instance_type, "must be from the list")
+  #     end
+  #   elsif name == "DataCenter"
+  #     if !user_name.present?
+  #       errors.add(:user_name, " can't be blank")
+  #     end
+  #
+  #     if !machines.present? or machines == "[]"
+  #       errors.add(:machines, " can't be blank")
+  #     end
+  #   end
+  #
+  # end
 
   def self.pagination(current_page, items_per_page)
     self.paginate(page: current_page, per_page: items_per_page).order("updated_at DESC")
   end
 
-  def getClustersOfType(type, project_id)
-    clusters = Cluster.where(:project_id=>project_id, :name => type)
-    return clusters
-  end
+  # def getClustersOfType(type, project_id)
+  #   clusters = Cluster.where(:project_id=>project_id, :name => type)
+  #   return clusters
+  # end
 
 end
