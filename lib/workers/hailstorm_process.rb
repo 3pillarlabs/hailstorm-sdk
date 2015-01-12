f8b2b3b0f579e153c1ec4e708d335f99859d5268
eash khatri <eash.khatri@3pillarglobal.com>
2015-01-12 15:52:28 +0530

    code restructuring done

--- lib/workers/hailstorm_process.rb
+++ lib/workers/hailstorm_process.rb
@@ -8,7 +8,7 @@ class HailstormProcess
 
   @@hailstorm_pool = {}
 
-  def perform(app_name, app_root_path, app_process, callback=nil, upload_directory_path=nil, project_id=nil, environment_data=nil)
+  def perform(app_name, app_root_path, app_process, project_id=nil, callback=nil, upload_directory_path=nil, environment_data=nil)
     puts "configure application"
     puts "app name : "+app_name
     puts "app root path : "+app_root_path
@@ -17,16 +17,10 @@ class HailstormProcess
     case app_process
       when 'setup'
         project_setup(app_name, app_root_path, callback, upload_directory_path, project_id, environment_data)
-      when 'start'
-        project_start(app_name, app_root_path, callback)
-      when 'stop'
-        project_stop(app_name, app_root_path, callback)
-      when 'abort'
-        project_abort(app_name, app_root_path, callback)
+      when 'start', 'stop', 'abort', 'terminate'
+        process_request(app_name, app_root_path, project_id, app_process, callback)
       when 'results'
-        project_results(app_name, app_root_path)
-      when 'terminate'
-        project_terminate(app_name, app_root_path, callback)
+        project_results(app_name, app_root_path, project_id)
     end
 
     puts "application configuration ended"
@@ -40,9 +34,14 @@ class HailstormProcess
       @@hailstorm_pool[project_id_str] = Hailstorm.application
     else
       puts "*** hailstorm object already exists in pool for project id :"+project_id_str
+      Hailstorm::Application.initialize!(app_name,app_boot_file_path)
+      @@hailstorm_pool[project_id_str] = Hailstorm.application
     end
 
     puts "2: project ids having objects in pool: "+@@hailstorm_pool.keys.inspect
+
+    puts "****current project: "
+    puts @@hailstorm_pool[project_id_str].current_project.inspect
   end
 
 
@@ -99,56 +98,30 @@ class HailstormProcess
     #now setup app configuration
     @@hailstorm_pool[project_id_str].interpret_command("setup")
 
-    # system ("curl #{callback}")
+    system ("curl #{callback}")
+
+    puts "application setup process ended"
   end
 
-  def project_start(app_name, app_root_path, callback)
-    puts "in start project worker of "+app_name
+  def process_request(app_name, app_root_path, project_id, command, callback)
+    project_id_str = project_id.to_s
+    puts "in "+command+" project worker of "+app_name
     app_boot_file_path = File.join(app_root_path, app_name, 'config/boot.rb')
     #Hailstorm::Application.initialize!(app_name,app_boot_file_path)
 
     set_hailstorm_in_pool_if_not_exists(project_id_str, app_name, app_root_path)
 
     #now setup app configuration
-    @@hailstorm_pool[project_id_str].interpret_command("start")
+    @@hailstorm_pool[project_id_str].interpret_command(command)
 
     puts "callback to: "+callback
     system ("curl #{callback}")
 
-    puts "application starting process ended"
+    puts "application "+command+" process ended"
   end
 
-  def project_stop(app_name, app_root_path, callback)
-    puts "in stop project worker of "+app_name
-    app_boot_file_path = File.join(app_root_path, app_name, 'config/boot.rb')
-    #Hailstorm::Application.initialize!(app_name,app_boot_file_path)
-
-    #now setup app configuration
-    Hailstorm.application.interpret_command("stop")
-
-    puts "callback to: "+callback
-    system ("curl #{callback}")
-
-    puts "application stopped process ended"
-  end
-
-  def project_abort(app_name, app_root_path, callback)
-    puts "in abort project worker of "+app_name
-    app_boot_file_path = File.join(app_root_path, app_name, 'config/boot.rb')
-    #Hailstorm::Application.initialize!(app_name,app_boot_file_path)
-
-    set_hailstorm_in_pool_if_not_exists(project_id_str, app_name, app_root_path)
-
-    #now setup app configuration
-    @@hailstorm_pool[project_id_str].interpret_command("abort")
-
-    puts "callback to: "+callback
-    system ("curl #{callback}")
-
-    puts "application aborting process ended"
-  end
-
-  def project_results(app_name, app_root_path)
+  def project_results(app_name, app_root_path, project_id)
+    project_id_str = project_id.to_s
     puts "in project results worker of "+app_name
     app_boot_file_path = File.join(app_root_path, app_name, 'config/boot.rb')
     #Hailstorm::Application.initialize!(app_name,app_boot_file_path)
@@ -157,98 +130,6 @@ class HailstormProcess
     Hailstorm.application.interpret_command("results report")
 
     puts "application result process ended"
-  end
-
-  def project_terminate(app_name, app_root_path, callback)
-    puts "in terminate project worker of "+app_name
-    set_hailstorm_in_pool_if_not_exists(project_id_str, app_name, app_root_path)
-    #now setup app configuration
-    @@hailstorm_pool[project_id_str].interpret_command("terminate")
-
-    puts "callback to: "+callback
-    system ("curl #{callback}")
-
-    puts "application terminating process ended"
-  end
-
-  def perform2(app_name, app_root_path, app_process, callback=nil, upload_directory_path=nil, project_id=nil, environment_data=nil)
-    project_id_str = project_id.to_s
-    puts "configure application"
-    puts "app name : "+app_name
-    puts "app root path : "+app_root_path
-    puts "project id : "+project_id_str
-    puts "callback url: "+callback
-
-    template_directory = File.join(Dir.pwd, 'lib', 'templates')
-
-
-    environment_template = Erubis::Eruby.new(File.read(template_directory+"/environment.erb"))
-    jmeter_template = Erubis::Eruby.new(File.read(template_directory+"/jmeter_config.erb"))
-    ec2_config_template = Erubis::Eruby.new(File.read(template_directory+"/ec2_config.erb"))
-    data_center_template = Erubis::Eruby.new(File.read(template_directory+"/data_center.erb"))
-
-    jmeter_config_str = environment_data['test_plans_data'].blank? ? "" : jmeter_template.result(:test_plans_data => environment_data['test_plans_data'])
-    ec2_config_str = environment_data['amazon_clouds_data'].blank? ? "" : ec2_config_template.result(:amazon_clouds_data => environment_data['amazon_clouds_data'])
-    data_center_config_str = environment_data['data_centers_data'].blank? ? "" : data_center_template.result(:data_centers_data => environment_data['data_centers_data'])
-
-    hailstormObj = Hailstorm::Application.new
-    app_directory = File.join(app_root_path, app_name)
-    if !File.directory?(app_directory)
-      #create app directory structure
-      hailstormObj.create_project(app_root_path,app_name)
-
-      #place log4j file in config directory of project
-      log_source_file_path = File.join(Dir.pwd, 'lib', 'templates/log4j.xml')
-      log_dest_file_path = File.join(app_directory, 'config/')
-      FileUtils.cp log_source_file_path, log_dest_file_path
-
-      #change database.properties file and add password to it
-      app_database_file_path = File.join(app_directory, 'config/database.properties')
-      dbtext = File.read(app_database_file_path)
-      File.write(app_database_file_path, dbtext.gsub(/password =/, "password = sa"))
-
-      #change GEM hailstorm path
-      app_gem_file_path = File.join(app_directory, 'Gemfile')
-      gemtext = File.read(app_gem_file_path)
-      File.write(app_gem_file_path, gemtext.gsub(/"hailstorm"/, '"hailstorm", :path=> "/home/ravish/Projects/hail-web/hailstorm-gem/"'))
-
-    end
-
-    #copy jmx file to app jmeter directory
-    sourcejmx_file_path = File.join(upload_directory_path, 'Jmx_Files', project_id.to_s, "/.")
-    destjmx_file_path = File.join(app_directory, 'jmeter/')
-
-    puts "**** copying JMX files from source: "+sourcejmx_file_path+" to destination: "+destjmx_file_path
-    FileUtils.cp_r sourcejmx_file_path, destjmx_file_path
-
-    #create enviroment file for app
-    env_file_path = File.join(app_directory, 'config/environment.rb')
-    envstr = environment_template.result(:jmeter_config => jmeter_config_str, :ec2_config => ec2_config_str, :data_center_config => data_center_config_str)
-    File.open(env_file_path, 'w') { |file| file.write(envstr) }
-
-    #setup database by initializing application
-    app_boot_file_path = File.join(app_directory, 'config/boot.rb')
-    #puts "app boot file path : "+app_boot_file_path
-    # puts "project ids having objects in pool: "+@@hailstorm_pool.keys.inspect
-    # if @@hailstorm_pool[project_id_str] == nil
-    #   puts "*** hailstorm object does not exists in pool for project id :"+project_id_str
-    #   Hailstorm::Application.initialize!(app_name,app_boot_file_path)
-    #   @@hailstorm_pool[project_id_str] = Hailstorm.application
-    # else
-    #   puts "*** hailstorm object already exists in pool for project id :"+project_id_str
-    # end
-    #
-    # puts "2: project ids having objects in pool: "+@@hailstorm_pool.keys.inspect
-
-    set_hailstorm_in_pool_if_not_exists(project_id_str, app_name, app_root_path)
-
-    #now setup app configuration
-    @@hailstorm_pool[project_id_str].interpret_command("setup")
-
-    puts "callback to: "+callback
-
-    # system ("curl #{callback}")
-    puts "application configuration ended"
   end
 
 end
