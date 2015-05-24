5aee483c7114d6cbf4e4682b197c6b5a189d4700
Sayantam Dey <sayantam@gmail.com>
2015-05-24 13:06:18 +0530

    Close btn text changed from 'x' to '&times'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,15 @@
 module ApplicationHelper
-  def bootstrap_class_for flash_type
+
+  def bootstrap_class_for(flash_type)
     case flash_type
-      when "success"
-        "alert-success"
-      when "error"
-        "alert-error"
-      when "alert"
-        "alert-block"
-      when "notice"
-        "alert-info"
+      when 'success'
+        'alert-success'
+      when 'error'
+        'alert-error'
+      when 'alert'
+        'alert-block'
+      when 'notice'
+        'alert-info'
       else
         flash_type.to_s
     end
@@ -16,10 +17,12 @@ module ApplicationHelper
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
-        concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
-        concat message
-      end)
+      concat(content_tag(:div, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') {
+               concat(content_tag(:button, class: 'close', data: { dismiss: 'alert' }, type: 'button', aria: {label: 'Close'}) {
+                        content_tag(:span, '&times;', {aria: {hidden: true}}, false)
+                      })
+               concat(message)
+             })
     end
     nil
   end
