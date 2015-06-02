cb1801bc4bea4fe2bec21b31426938b4f118f488
Sayantam Dey <sayantam@gmail.com>
2015-06-02 08:06:18 +0530

    Common form attribute error helper; flash message icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,9 @@ module ApplicationHelper
       when 'success'
         'alert-success'
       when 'error'
-        'alert-error'
+        'alert-danger'
       when 'alert'
-        'alert-block'
+        'alert-warning'
       when 'notice'
         'alert-info'
       else
@@ -15,13 +15,43 @@ module ApplicationHelper
     end
   end
 
+  def glyphicon_for(msg_type)
+    icon = case msg_type.to_sym
+             when :success
+               'glyphicon-ok-sign'
+             when :error
+               'glyphicon-remove-sign'
+             when :alert
+               'glyphicon-warning-sign'
+             else
+               'glyphicon-info-sign'
+           end
+
+    "glyphicon #{icon}"
+  end
+
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
       concat(content_tag(:div, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') {
-               concat(content_tag(:button, class: 'close', data: { dismiss: 'alert' }, type: 'button', aria: {label: 'Close'}) {
+               concat(content_tag(:button, class: 'close', data: {dismiss: 'alert'}, type: 'button', aria: {label: 'Close'}) {
                         content_tag(:span, '&times;', {aria: {hidden: true}}, false)
                       })
-               concat(message)
+               concat(content_tag(:span, '', {class: glyphicon_for(msg_type)}))
+               concat(" #{message}")
+             })
+    end
+    nil
+  end
+
+  # @param [ActiveRecord::Base] model
+  # @param [Symbol] attribute
+  def errors_if_present(model, attribute)
+    if model.errors.include?(attribute)
+      concat(content_tag(:p, {class: 'text-danger', aria: {role: 'alert'}}, true) {
+               concat(content_tag(:span, '', class: 'glyphicon glyphicon-exclamation-sign', aria: {hidden: 'true'}))
+               concat(content_tag(:span, 'Error:', class: 'sr-only'))
+               concat(' ')
+               concat(model.errors.full_messages_for(attribute).join('. '))
              })
     end
     nil
