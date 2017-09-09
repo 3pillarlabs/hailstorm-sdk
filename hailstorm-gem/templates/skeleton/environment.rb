86428048dd7164b48371f1dc7fb6bb080f4173af
Sayantam Dey <sayantam@gmail.com>
2017-09-10 00:20:18 +0530

    changed wiki reference

--- hailstorm-gem/templates/skeleton/environment.rb
+++ hailstorm-gem/templates/skeleton/environment.rb
@@ -1,5 +1,5 @@
 # Hailstorm configuration
 Hailstorm.application.config do |config|
   # add your configuration here, for help check out
-  # https://helpdesk.3pillarglobal.com/clientwiki/display/3PillarLabs/Hailstorm+User+Guide
+  # https://github.com/3pillarlabs/hailstorm-sdk/wiki
 end
