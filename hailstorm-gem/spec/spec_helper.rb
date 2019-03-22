60a5b60e95373a197366fbab383659185c5f06f0
Sayantam Dey <sayantam@gmail.com>
2019-03-22 21:13:01 +0530

    Set root to $project_dir/build

--- hailstorm-gem/spec/spec_helper.rb
+++ hailstorm-gem/spec/spec_helper.rb
@@ -26,8 +26,11 @@ RSpec.configure do |config|
   config.order = 'random'
 
   config.prepend_before(:suite) do
-    FileUtils.mkdir_p(File.join(File.expand_path('../..', __FILE__), Hailstorm.tmp_dir))
-    middleware = Hailstorm::Initializer.create_middleware('hailstorm_spec', __FILE__, {
+    build_path = File.join(File.expand_path('../..', __FILE__), 'build')
+    FileUtils.mkdir_p(build_path)
+    boot_file_path = File.join(build_path, 'config', 'boot.rb')
+    FileUtils.mkdir_p(File.join(build_path, Hailstorm.tmp_dir))
+    middleware = Hailstorm::Initializer.create_middleware('hailstorm_spec', boot_file_path, {
       adapter: 'jdbcmysql',
       database: 'hailstorm_gem_test',
       username: 'hailstorm_dev',
