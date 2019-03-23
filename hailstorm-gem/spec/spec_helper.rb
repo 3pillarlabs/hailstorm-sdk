db74dcfc02cc14d217f74100136896761b5413b2
Sayantam Dey <sayantam@gmail.com>
2019-03-23 17:55:54 +0530

    Create temporary artifacts to check cleanup

--- hailstorm-gem/spec/spec_helper.rb
+++ hailstorm-gem/spec/spec_helper.rb
@@ -30,6 +30,8 @@ RSpec.configure do |config|
     FileUtils.mkdir_p(build_path)
     boot_file_path = File.join(build_path, 'config', 'boot.rb')
     FileUtils.mkdir_p(File.join(build_path, Hailstorm.tmp_dir))
+    FileUtils.mkdir_p(File.join(build_path, Hailstorm.tmp_dir, 'remove_dir'))
+    FileUtils.touch(File.join(build_path, Hailstorm.tmp_dir, 'remove_file'))
     middleware = Hailstorm::Initializer.create_middleware('hailstorm_spec', boot_file_path, {
       adapter: 'jdbcmysql',
       database: 'hailstorm_gem_test',
