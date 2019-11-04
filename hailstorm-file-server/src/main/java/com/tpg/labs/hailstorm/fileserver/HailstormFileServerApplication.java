f66c480a20dee7e0984489cbb9649aecf699f44b
Sayantam Dey <sayantam@gmail.com>
2019-11-04 17:00:46 +0530

    Initial commit for file server

---
+++ hailstorm-file-server/src/main/java/com/tpg/labs/hailstorm/fileserver/HailstormFileServerApplication.java
@@ -0,0 +1,13 @@
+package com.tpg.labs.hailstorm.fileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HailstormFileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HailstormFileServerApplication.class, args);
+	}
+
+}
