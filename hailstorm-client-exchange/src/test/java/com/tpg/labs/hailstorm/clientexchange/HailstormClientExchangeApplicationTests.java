89be80e59bc2f28bc23d3dc3c7936e151b6e227f
Sayantam Dey <sayantam@gmail.com>
2019-11-15 18:15:18 +0530

    Initial commit for client exchange

---
+++ hailstorm-client-exchange/src/test/java/com/tpg/labs/hailstorm/clientexchange/HailstormClientExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpg.labs.hailstorm.clientexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HailstormClientExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
