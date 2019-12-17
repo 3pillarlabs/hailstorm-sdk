fb74c761fa63f37929971986eb887b7fc5ae7fdf
Sayantam Dey <sayantam@gmail.com>
2019-12-17 22:43:03 +0530

    Fix date to ISO format

--- hailstorm-api/api/projects.rb
+++ hailstorm-api/api/projects.rb
@@ -5,13 +5,28 @@ require 'config/db/seed'
 get '/projects' do
   JSON.dump(Seed::DB[:projects].map { |project|
     project_with_execution_cycle = project.clone
-    project_with_execution_cycle[:currentExecutionCycle] = Seed::DB[:executionCycles]
-                                          .find { |x| x[:projectId] == project[:id] && x[:stoppedAt].nil? }
+    currentExecutionCycle = Seed::DB[:executionCycles].find { |x| x[:projectId] == project[:id] && x[:stoppedAt].nil? }
+    unless currentExecutionCycle.nil?
+      project_with_execution_cycle[:currentExecutionCycle] = currentExecutionCycle.clone
+      project_with_execution_cycle[:currentExecutionCycle][:startedAt] =
+          project_with_execution_cycle[:currentExecutionCycle][:startedAt].strftime('%Y-%m-%dT%H:%M:%S.%L%z')
+    end
+
+    unless project_with_execution_cycle[:lastExecutionCycle].nil?
+      project_with_execution_cycle[:lastExecutionCycle] = project_with_execution_cycle[:lastExecutionCycle].clone
+      project_with_execution_cycle[:lastExecutionCycle][:startedAt] =
+          project_with_execution_cycle[:lastExecutionCycle][:startedAt].strftime('%Y-%m-%dT%H:%M:%S.%L%z')
+      if project_with_execution_cycle[:lastExecutionCycle][:stoppedAt]
+        project_with_execution_cycle[:lastExecutionCycle][:stoppedAt] =
+            project_with_execution_cycle[:lastExecutionCycle][:stoppedAt].strftime('%Y-%m-%dT%H:%M:%S.%L%z')
+      end
+    end
+
     project_with_execution_cycle
   })
 end
 
 get '/projects/:id' do |id|
-  found_project = Seed::DB[:projects].select { |project| project[:id] == id.to_s.to_i }.first
+  found_project = Seed::DB[:projects].find { |project| project[:id] == id.to_s.to_i }
   found_project ? JSON.dump(found_project) : not_found
 end
