98c3906f3cf65a2e36b90f682bcd178412359f64
Sayantam Dey <sayantam@gmail.com>
2019-12-17 23:42:44 +0530

    Exchange time in milliseconds since Epoch

--- hailstorm-api/api/projects.rb
+++ hailstorm-api/api/projects.rb
@@ -3,22 +3,25 @@ require 'json'
 require 'config/db/seed'
 
 get '/projects' do
-  JSON.dump(Seed::DB[:projects].map { |project|
+  JSON.dump(Seed::DB[:projects].map {
+      # @type [Hash] project
+      |project|
+
     project_with_execution_cycle = project.clone
-    currentExecutionCycle = Seed::DB[:executionCycles].find { |x| x[:projectId] == project[:id] && x[:stoppedAt].nil? }
-    unless currentExecutionCycle.nil?
-      project_with_execution_cycle[:currentExecutionCycle] = currentExecutionCycle.clone
+    current_execution_cycle = Seed::DB[:executionCycles].find { |x| x[:projectId] == project[:id] && x[:stoppedAt].nil? }
+    unless current_execution_cycle.nil?
+      project_with_execution_cycle[:currentExecutionCycle] = current_execution_cycle.clone
       project_with_execution_cycle[:currentExecutionCycle][:startedAt] =
-          project_with_execution_cycle[:currentExecutionCycle][:startedAt].strftime('%Y-%m-%dT%H:%M:%S.%L%z')
+          project_with_execution_cycle[:currentExecutionCycle][:startedAt].to_i * 1000
     end
 
     unless project_with_execution_cycle[:lastExecutionCycle].nil?
       project_with_execution_cycle[:lastExecutionCycle] = project_with_execution_cycle[:lastExecutionCycle].clone
       project_with_execution_cycle[:lastExecutionCycle][:startedAt] =
-          project_with_execution_cycle[:lastExecutionCycle][:startedAt].strftime('%Y-%m-%dT%H:%M:%S.%L%z')
+          project_with_execution_cycle[:lastExecutionCycle][:startedAt].to_i * 1000
       if project_with_execution_cycle[:lastExecutionCycle][:stoppedAt]
         project_with_execution_cycle[:lastExecutionCycle][:stoppedAt] =
-            project_with_execution_cycle[:lastExecutionCycle][:stoppedAt].strftime('%Y-%m-%dT%H:%M:%S.%L%z')
+            project_with_execution_cycle[:lastExecutionCycle][:stoppedAt].to_i * 1000
       end
     end
 
