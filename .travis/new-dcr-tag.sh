b1f15159722665aaeaa3ae2fc05cecf2b4d42788
Sayantam Dey <sayantam@gmail.com>
2020-04-09 11:58:41 +0530

    Change exit code to zero if tag is in DCR

--- .travis/new-dcr-tag.sh
+++ .travis/new-dcr-tag.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Exits with zero status code if the docker image/name:tag is not in the container registry.
+# Exits with zero status code if the docker image/name:tag is in the container registry.
 
 docker_id=$1
 docker_repo=$2
@@ -8,5 +8,3 @@ docker_tag=$3
 curl -sf "https://registry.hub.docker.com/v2/repositories/${docker_id}/${docker_repo}/tags/" | \
 jq -r '."results"[]["name"]' | \
 grep "${docker_tag}" >/dev/null 2>&1
-
-test $? -ne 0
